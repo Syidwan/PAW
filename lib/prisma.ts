b7131ade98c5246c9313d25fa78769f9b128cb29
Syidwan <nirvvan.rasyid@gmail.com>
2024-12-07 10:54:36 +0700

    fix: coba

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client";
+
+// Menambahkan global variable untuk menyimpan instance PrismaClient
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// Membuat instance PrismaClient atau menggunakan yang sudah ada jika di dev mode
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// Memastikan Prisma hanya diinisialisasi sekali di environment selain production
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
