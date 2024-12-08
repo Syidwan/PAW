344fbec9f19635c81a56da91298260bc42ac7d66
Syidwan <nirvvan.rasyid@gmail.com>
2024-12-08 12:42:26 +0700

    fix error

--- types/authjs.d.ts
+++ types/authjs.d.ts
@@ -1,5 +1,12 @@
 import { type DefaultSession } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { AdapterUser } from "@auth/core/adapters"; 
+
+declare module "@auth/core/adapters" {
+   interface AdapterUser {
+     role: string;  // Menambahkan properti 'role' pada tipe AdapterUser
+   }
+ }
 
 declare module "next-auth" {
    interface Session{
