5009db53174fd363284bcca4e61b62163f704335
Syidwan <nirvvan.rasyid@gmail.com>
2024-12-10 00:13:42 +0700

    SignIn Google

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
 import FormLogin from "@/components/auth/form-login";
+import { GoogleButton } from "@/components/auth/social-button";
 
 const Login = () => {
   return (
     <div className="p-6 space-y-4">
       <h1 className="text-2xl font-bold text-gray-900">Sign In Account</h1>
       <FormLogin />
+      <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300">
+        <p className="mx-4 mb-0 text-center font-semibold text-gray-600">Or</p>
+      </div>
+      <GoogleButton/>
     </div>
   );
 };
---
+++ components/user-table.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { getUsers } from "@/lib/data";
+import { formatDate } from "@/lib/utils";
+const UserTable = async () => {
+  const users = await getUsers();
+  if (!users?.length) return <h1>No User Data</h1>;
+  return (
+    <table className="w-full bg-white mt-3">
+      <thead className="border-b border-gray-100">
+        <tr>
+          <th className="py-3 px-6 text-left text-sm">Name</th>
+          <th className="py-3 px-6 text-left text-sm">Email</th>
+          <th className="py-3 px-6 text-left text-sm">Role</th>
+          <th className="py-3 px-6 text-left text-sm">Created At</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td className="py-3 px-6">{user.name}</td>
+            <td className="py-3 px-6">{user.email}</td>
+            <td className="py-3 px-6">{user.role}</td>
+				<td className="py-3 px-6">{ formatDate(user.createdAt.toString())}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
