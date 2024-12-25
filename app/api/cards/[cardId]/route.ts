b53d6c884400b4b1ddc9dd1655808e7cab3d006c
Syidwan <nirvvan.rasyid@gmail.com>
2024-12-25 18:26:32 +0700

    try any

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: { cardId: string } }
+  { params }: { params: any }
 ) {
   try {
     const { cardId } = params;
